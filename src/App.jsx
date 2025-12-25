df9f9555713b8b3d39a09dbeccaac04773de537a
Gift Udofiah <giftudofiah@gmail.com>
2025-12-25 13:32:32 +0100

    recognition one

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navigation from './components/navigation/Navigation'
+import Logo from './components/logo/Logo'
+import ImageLinkForm from './components/imagelink/ImageLinkForm'
+
+function App() {
+
+  return (
+    <div>
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      {/* 
+      
+      <FaceRecognition /> */}
+    </div>
+  )
+}
+
+export default App
