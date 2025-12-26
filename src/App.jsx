3025f423e330037a105b168e61537a4f72fb85fc
Gift Udofiah <giftudofiah@gmail.com>
2025-12-26 17:14:49 +0100

    ui done for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import './App.css'
-import Navigation from './components/navigation/Navigation'
-import Logo from './components/logo/Logo'
-import ImageLinkForm from './components/imagelink/ImageLinkForm'
+import './App.css';
+import Particles from "@tsparticles/react";
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
+import Rank from './components/rank/Rank';
+import ImageLinkForm from './components/imagelink/ImageLinkForm';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 function App() {
 
   return (
     <div>
+      <Particles params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* 
       
